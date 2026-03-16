bb26103e6cbfc95f355d127221f5511b768d5364
Rangel <96200914+rangelbjordao@users.noreply.github.com>
2026-03-16 15:10:25 -0300

    'Add-Migration' e 'Update-Database' executados

---
+++ FiapEstoque/FiapEstoque/Migrations/20260316180755_CriarTabelaItens.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FiapEstoque.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriarTabelaItens : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "T_Itens",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    Nome = table.Column<string>(type: "NVARCHAR2(100)", maxLength: 100, nullable: false),
+                    Quantidade = table.Column<int>(type: "NUMBER(10)", nullable: false),
+                    PrecoUnitario = table.Column<decimal>(type: "decimal(10,2)", nullable: false),
+                    Categoria = table.Column<string>(type: "NVARCHAR2(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_T_Itens", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "T_Itens");
+        }
+    }
+}
