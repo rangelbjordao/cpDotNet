859520d4493012a19bcbfa373173c41cac0ad905
Rangel <96200914+rangelbjordao@users.noreply.github.com>
2026-03-16 09:52:37 -0300

    Model Item

---
+++ FiapEstoque/FiapEstoque/Model/Item.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FiapEstoque.Models
+{
+    [Table("T_ITens")]
+    public class Item
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nome { get; set; } = string.Empty;
+
+        [Required]
+        public int Quantidade { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal PrecoUnitario { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Categoria { get; set; } = string.Empty;
+    }
+}
