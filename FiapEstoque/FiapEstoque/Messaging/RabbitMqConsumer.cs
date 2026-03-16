276f9b70b70c59f50fa5c55f9b12dd75023e4967
Rangel <96200914+rangelbjordao@users.noreply.github.com>
2026-03-16 18:05:33 -0300

    Create RabbitMqConsumer.cs

---
+++ FiapEstoque/FiapEstoque/Messaging/RabbitMqConsumer.cs
@@ -0,0 +1,25 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace FiapEstoque.Messaging
+{
+    public class RabbitMqConsumer : BackgroundService
+    {
+        protected override async Task ExecuteAsync(CancellationToken ct)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            var conn = factory.CreateConnection();
+            var ch = conn.CreateModel();
+            ch.QueueDeclare("item-criado", false, false, false, null);
+            var consumer = new EventingBasicConsumer(ch);
+            consumer.Received += (s, e) => {
+                var msg = Encoding.UTF8.GetString(e.Body.ToArray());
+                Console.WriteLine($"[Consumer] Recebido: {msg}");
+                ch.BasicAck(e.DeliveryTag, false); // ACK manual
+            };
+            ch.BasicConsume("item-criado", false, consumer); // autoAck=false
+            await Task.Delay(Timeout.Infinite, ct); // fica vivo
+        }
+    }
+}
