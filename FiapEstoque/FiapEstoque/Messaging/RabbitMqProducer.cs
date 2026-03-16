0416a6294add456883870d07da684d7135bb4fd9
Rangel <96200914+rangelbjordao@users.noreply.github.com>
2026-03-16 18:02:51 -0300

    RabbitMqProducer

---
+++ FiapEstoque/FiapEstoque/Messaging/RabbitMqProducer.cs
@@ -0,0 +1,27 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json;
+
+namespace FiapEstoque.Messaging;
+
+    public static class RabbitMqProducer
+{
+    public static void Publish<T>(
+    T message)
+    {
+        var factory = new ConnectionFactory
+        { HostName = "localhost" };
+        using var conn =
+        factory.CreateConnection();
+        using var ch =
+        conn.CreateModel();
+        ch.QueueDeclare("item-criado",
+        false, false, false, null);
+        var json = JsonSerializer
+        .Serialize(message);
+        var body = Encoding.UTF8
+        .GetBytes(json);
+        ch.BasicPublish("",
+        "item-criado", null, body);
+    }
+}
