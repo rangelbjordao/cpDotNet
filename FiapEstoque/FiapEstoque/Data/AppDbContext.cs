5fbbc879e26fd6b84590f360949ba7b15172cf83
Rangel <96200914+rangelbjordao@users.noreply.github.com>
2026-03-16 14:14:26 -0300

    AppDbContext

---
+++ FiapEstoque/FiapEstoque/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using FiapEstoque.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FiapEstoque.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Item> Itens { get; set; }
+    }
+}
