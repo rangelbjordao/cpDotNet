1d6edcd1ad4555af185a9106288b60d66c74f65c
Rangel <96200914+rangelbjordao@users.noreply.github.com>
2026-03-16 15:19:41 -0300

    ItensController

---
+++ FiapEstoque/FiapEstoque/Controllers/ItensController.cs
@@ -0,0 +1,68 @@
+﻿using FiapEstoque.Data;
+using FiapEstoque.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+namespace FiapEstoque.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+public class ItensController
+ : ControllerBase
+{
+    private readonly AppDbContext _ctx;
+    public ItensController(
+    AppDbContext ctx)
+    => _ctx = ctx;
+
+    [HttpGet]
+    public async Task<IActionResult>
+    GetAll()
+    => Ok(await _ctx.Itens
+    .ToListAsync());
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult>
+    GetById(int id)
+    {
+        var p = await _ctx.Itens
+        .FindAsync(id);
+        return p is null
+        ? NotFound() : Ok(p);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult>
+ Create(Item item)
+    {
+        _ctx.Itens.Add(item);
+        await _ctx.SaveChangesAsync();
+        return CreatedAtAction(
+        nameof(GetById),
+        new { id = item.Id },
+        item);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult>
+ Update(int id, Item p)
+    {
+        if (id != p.Id)
+            return BadRequest();
+        _ctx.Entry(p).State =
+        EntityState.Modified;
+        await _ctx.SaveChangesAsync();
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult>
+ Delete(int id)
+    {
+        var p = await _ctx.Itens
+        .FindAsync(id);
+        if (p is null)
+            return NotFound();
+        _ctx.Itens.Remove(p);
+        await _ctx.SaveChangesAsync();
+        return NoContent();
+    }
+}
